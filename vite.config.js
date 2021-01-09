d42814e607bebcdee7ab39fd80b32687ff388880
Niko <bluepspower@163.com>
2021-01-09 17:25:35 +0800

    chore: udpate vite-config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
 const { createVuePlugin } = require('vite-plugin-vue2')
 
-const config = defineConfig({
-  plugins: [ createVuePlugin() ]
-})
-
-export default config
+export default {
+  plugins: [
+    createVuePlugin()
+  ]
+}
